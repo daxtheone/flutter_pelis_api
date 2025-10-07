de4f1ca0df00be280506f39e48fc7ef55c05b025
Daniel Rico <danielricomaya@gmail.com>
2025-10-07 17:54:27 -0300

    actualzaicion

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+
 import 'package:pelis_api/config/router/app_router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:pelis_api/config/theme/app_theme.dart';
 
-void main() {
+Future<void> main() async{
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
