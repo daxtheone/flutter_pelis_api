2b37839228171e91591eb18cfac25161f0555c24
Daniel Rico <danielricomaya@gmail.com>
2025-11-19 10:50:15 -0300

    actualziacion

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pelis_api/domain/entities/movie.dart';
@@ -24,15 +25,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
           widget.movieId,
         ); // siempre q estamos en metodos usamos el read
 
-    ref
-        .read(actorsByMovieProvider.notifier)
-        .loadActors(
-      widget.movieId,
-    );
-
-
-
-
+    ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
   }
 
   @override
@@ -126,44 +119,57 @@ class _MovieDetails extends StatelessWidget {
   }
 }
 
-
 class _ActorsByMovie extends ConsumerWidget {
   final String movieId;
   const _ActorsByMovie({required this.movieId});
 
   @override
   Widget build(BuildContext context, ref) {
-
     final actorsByMovie = ref.watch(actorsByMovieProvider);
-    if(actorsByMovie[movieId]== null){
-      return CircularProgressIndicator(strokeWidth: 2,);
+    if (actorsByMovie[movieId] == null) {
+      return CircularProgressIndicator(strokeWidth: 2);
     }
     final actors = actorsByMovie[movieId]!;
     return SizedBox(
       height: 600,
-      child: ListView.builder(itemBuilder: (context, index){
-        final actor = actors[index];
-        return Container( padding: const EdgeInsets.all(8.0), width: 135,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.network(actor.profilePath, height: 180, width: 135, fit: BoxFit.cover,),
-              ),
-              SizedBox(height: 5,),
-              Text(actor.name, maxLines: 2,),
-              Text(actor.character ?? '', maxLines: 2, style: TextStyle(fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis),),
-
-
-            ],
-          )
-        );
-      },itemCount: actors.length, scrollDirection: Axis.horizontal,),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+          return Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    actor.profilePath,
+                    height: 180,
+                    width: 135,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(actor.name, maxLines: 2),
+                Text(
+                  actor.character ?? '',
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        itemCount: actors.length,
+        scrollDirection: Axis.horizontal,
+      ),
     );
   }
 }
-
 
 class _CustomSliverApp extends StatelessWidget {
   final Movie movie;
@@ -187,7 +193,14 @@ class _CustomSliverApp extends StatelessWidget {
         background: Stack(
           children: [
             SizedBox.expand(
-              child: Image.network(movie.posterPath, fit: BoxFit.cover),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress != null) return SizedBox();
+                  return FadeIn(child: child);
+                },
+              ),
             ),
             SizedBox.expand(
               child: DecoratedBox(
