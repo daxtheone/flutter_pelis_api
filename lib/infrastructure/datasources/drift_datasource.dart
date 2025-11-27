f2ad5061c3115f1de9027c75ab8cce01973c41f7
Daniel Rico <danielricomaya@gmail.com>
2025-11-27 15:48:58 -0300

    actualaicion

--- lib/infrastructure/datasources/drift_datasource.dart
+++ lib/infrastructure/datasources/drift_datasource.dart
@@ -1,21 +1,19 @@
-
-
-
+import 'package:drift/drift.dart' as drift;
 import 'package:pelis_api/config/database/database.dart';
 import 'package:pelis_api/domain/datasources/local_storage_datasrouce.dart';
 import 'package:pelis_api/domain/entities/movie.dart';
 
-class DriftDatasource extends LocalStorageDatasource{
-
+class DriftDatasource extends LocalStorageDatasource {
   final AppDatabase database;
 
-  DriftDatasource([AppDatabase? databaseToUse]) : database = databaseToUse ?? db;
-
+  DriftDatasource([AppDatabase? databaseToUse])
+    : database = databaseToUse ?? db;
 
   @override
-  Future<bool> isFavoriteMovie(int movieId) async{
+  Future<bool> isFavoriteMovie(int movieId) async {
     // Construir el query
-    final query = database.select(database.favoriteMovies)..where((table)=>table.movieId.equals(movieId));
+    final query = database.select(database.favoriteMovies)
+      ..where((table) => table.movieId.equals(movieId));
 
     // Ejecutar el query
     final favoriteMovie = await query.getSingleOrNull();
@@ -25,17 +23,37 @@ class DriftDatasource extends LocalStorageDatasource{
   }
 
   @override
-  Future<List<Movie>> loadFavoriteMovies({int limit = 10, int offset = 0}) {
-    // TODO: implement loadFavoriteMovies
-    throw UnimplementedError();
+  Future<List<Movie>> loadFavoriteMovies({int limit = 10, int offset = 0}) async{
+    // creamos el query
+    final query = database.select(database.favoriteMovies)..limit(limit,offset: offset);
+
+    //Ejecutamos el query
+    final favoriteMovieRows = await query.get();
+    final movies = favoriteMovieRows.map((row)=> Movie(adult: false, backdropPath: row.backdropPath, genreIds: [], id: row.movieId, originalLanguage: '', originalTitle: row.originalTitle, overview: '', popularity: 0, posterPath: row.posterPath, releaseDate: DateTime.now(), title: row.title, video: false, voteAverage: row.voteAverage, voteCount: 0)).toList();
+    return movies;
   }
 
   @override
-  Future<void> toggleFavoriteMovie(Movie movie) async{
+  Future<void> toggleFavoriteMovie(Movie movie) async {
     final isFavorite = await isFavoriteMovie(movie.id);
-    if(isFavorite){
-      final deleteQuery = database.delete(database)
+    if (isFavorite) {
+      final deleteQuery = database.delete(database.favoriteMovies)
+        ..where((table) => table.movieId.equals(movie.id));
+      await deleteQuery.go();
+      return;
     }
+
+    await database
+        .into(database.favoriteMovies)
+        .insert(
+          FavoriteMoviesCompanion.insert(
+            movieId: movie.id,
+            backdropPath: movie.backdropPath,
+            originalTitle: movie.originalTitle,
+            posterPath: movie.posterPath,
+            title: movie.title,
+            voteAverage: drift.Value(movie.voteAverage),
+          ),
+        );
   }
-  
 }
